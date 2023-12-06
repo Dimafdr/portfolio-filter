31db3d3e3bcd8b2320ee7278fbdec1b685132466
вашÐDimafdr <Dimafdr@yandex.ru>
2023-12-06 13:44:55 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './components/Portfolio'
+import Portfolio from "./components/Portfolio";
+
+
+function App() {
+  return (
+    <Portfolio />
+  );
+}
+
+export default App;
